4f2fbc646d69db71282abe7510310457c5a095f4
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-04-28 18:07:14 +0530

    added procfile and requirements.txt

--- covid/settings.py
+++ covid/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^==n&fwzs1-)uarfyqp55kfx-=+pz2m8#!9c#(!kpawdv9@ylr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
