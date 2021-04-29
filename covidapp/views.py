9da1b30a30012b8159441b2f462edc1e30805bac
Daniyal Dolare <daniyal.dolare@gmail.com>
2021-04-29 21:42:31 +0530

    code refracted

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
 import requests
 import json
+
 url = "https://covid-193.p.rapidapi.com/statistics"
 
 headers = {
     'x-rapidapi-host': "covid-193.p.rapidapi.com",
     'x-rapidapi-key': "d9cbda2f6dmshc2bba4bd0e71c1ap1ab42fjsnc01c43dd2e87",
-
     }
 
 response = requests.request("GET", url, headers=headers).json()
 
-
-# Create your views here.
-
-## Function to get Data from response and return the dictionary
 def getData(index,countryList):
+
+    ''' Function to get Data of particular country from response and return the dictionary '''
+
     selectedCountry = response['response'][index]['country']
     new = response['response'][index]['cases']['new'] if response['response'][index]['cases']['new'] else 0
     active = response['response'][index]['cases']['active'] if response['response'][index]['cases']['active'] else 0
@@ -29,12 +28,10 @@ def getData(index,countryList):
 def index(request):
 
     countryList = []
-    worldwide = {}
-    context = {}
-    noofresults = int(response['results'])
+    noOfResults = int(response['results'])
     allStatIndex = 0
     
-    for x in range(0, noofresults):
+    for x in range(0, noOfResults):
         countryList.append(response['response'][x]['country'])
         if response['response'][x]['country']=='All':
             allStatIndex = x
@@ -42,15 +39,14 @@ def index(request):
     countryList.sort()
 
     if request.method=="POST":
-        print('in if')
         selectedcountry = request.POST['selectedcountry']
-        noofresults = int(response['results'])
-        for x in range(0,noofresults):
+        for x in range(0,noOfResults):
             if selectedcountry == response['response'][x]['country']:
                 context=getData(x,countryList)
         return render(request, 'index.html', context)
 
-    context = getData(allStatIndex,countryList)
+    # get total data of all worldwide and render at start 
+    context = getData(allStatIndex,countryList) 
     return render(request,'index.html',context)
 
     
